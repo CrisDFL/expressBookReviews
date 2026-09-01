67f4136e7571c8faeaabe88257564019409e93d6
CrisDFL <cristhiandavidfl@gmail.com>
2026-09-01 16:14:25 -0400

    autenticacion JWT

--- final_project/index.js
+++ final_project/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
+const { authenticated } = require('./router/auth_users.js');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
@@ -11,7 +12,22 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+
+    if(req.session.authenticated){
+        let token = req.session.authenticated['accessToken'];
+
+        jwt.verify(token, 'access', (err, user) => {
+            if(!err){
+                req.user = user;
+                next();
+                res.send(`Welcome user ${user}`);
+            } else {
+                return res.status(403).json({ message: "User not authenticated" });
+            }
+        });
+    } else {
+        return res.status(403).json({ message: "User not logged in" });
+    }
 });
  
 const PORT =5000;
--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,17 +6,60 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+    let validateUsername = users.filter(user => user.username === username);
+
+    if(validateUsername.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    let validateAuth = users.filter(user => user.username === username && user.password === password);
+
+    if(validateAuth.length > 0){
+        return true;
+    } else {
+        return false;
+    }
 }
+
+app.post('/register', (req, res) => {
+    let {username, password} = req.body;
+
+    if(username && password){
+        if(!isValid){
+            users.push({"username": username, "password": password});
+            return res.status(200).json({message: "User successfully registered. Now you can login"});
+        } else {
+            return res.status(404).json({message: "User already exists!"});
+        }
+    }
+     return res.status(404).json({message: "Unable to register user."});
+});
+
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+    let { username, password } = req.body;
+
+    if(authenticatedUser(username, password)) {
+        let token = jwt.sign(
+            {data: password},
+            'access',
+            {expiresIn: 60 * 60}
+        );
+
+        req.session.authenticated = {
+            token, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
+
 });
 
 // Add a book review
